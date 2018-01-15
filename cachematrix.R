c23735b5198313dbe33d139953a74e6ce58e1c05
Jorge Martinez <jernesto87@gmail.com>
2018-01-14 20:16:21 -0500

    Uploading Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The "object" vector where the values calculated will be stored and retrieved as required
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solveM) i <<- solveM
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Based on the example provided in the instructions of this assignment and the same idea of
+## validating if there is already a cached copy of the matrix on memory. The "object" 
+## vector is used as a repository to check if the value is available (and it is the same
+## input matrix) or it would need to be re-calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("Retrieving already cached matrix")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
